e3aad70901e99646641b20e77096cb093332500f
Rashedul Arefin Ifty <r.a.ifty2001@gmail.com>
2023-12-09 23:07:17 +0600

    JobCart (Demo Data)

--- src/components/JobCard/index.jsx
+++ src/components/JobCard/index.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import dayjs from 'dayjs'
 
-function JobCard() {
+function JobCard(props) 
+{
+    const skills = ["Javascript", "React", "Nodejs"];
+    const date1 = dayjs(Date.now());
+    const diffInDays = date1.diff('2023-10-28','day');
   return (
     <div>
-	jobcard
+        <div>
+            <div>
+                <h1>Data Scientist</h1>
+                <p>Full Time &#x2022; Fresher &#x2022; In-Office</p>
+                <div>
+                    {skills.map((skill) => (
+                        <p>{skill}</p>
+                    ))}
+                </div>
+            </div>
+            <div>
+                <p> Posted {diffInDays} ago</p>
+                    <button>Apply</button>
+            </div>
+        </div>
     </div>
   )
 }
