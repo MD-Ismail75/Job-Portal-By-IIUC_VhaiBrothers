6b5829b4b0bb0163b0cc2fba818524d007d786e6
ismail <ismail220285@gmail.com>
2023-12-09 21:10:58 +0600

    SearchBar Feature Added

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 function SearchBar() {
   return (
     <div>
-	searchbar
+	     <select>
+       <option value="" disabled hidden>Job Role</option>
+            <option value="iOS Developer">iOS Developer</option>
+            <option value="Frontend Developer">Frontend Developer</option>
+            <option value="Backend Developer">Backend Developer</option>
+            <option value="Android Developer">Android Developer</option>
+            <option value="Developer Advocate">Developer Advocate</option>
+       </select>
     </div>
   )
 }
