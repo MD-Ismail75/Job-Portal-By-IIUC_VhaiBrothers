c17a52ae6ea85b9156c0f48d98f86dd8c57a6bef
ismail <ismail220285@gmail.com>
2023-12-09 22:14:44 +0600

    Job Type Create

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -3,14 +3,33 @@ import React from 'react'
 function SearchBar() {
   return (
     <div>
-	     <select>
-       <option value="" disabled hidden>Job Role</option>
+	     <select className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
+            <option value="" disabled hidden>Job Role</option>
             <option value="iOS Developer">iOS Developer</option>
             <option value="Frontend Developer">Frontend Developer</option>
             <option value="Backend Developer">Backend Developer</option>
             <option value="Android Developer">Android Developer</option>
             <option value="Developer Advocate">Developer Advocate</option>
-       </select>
+        </select>
+        <select className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
+            <option value="" disabled hidden>Job Type</option>
+            <option value="Full Time">Full Time</option>
+            <option value="Part Time">Part Time</option>
+            <option value="Contract">Contract</option>
+        </select>
+        <select className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
+            <option value="" disabled hidden>Location</option>
+            <option value="Remote">Remote</option>
+            <option value="In-Office">In-Office</option>
+            <option value="Hybrid">Hybrid</option>
+        </select>
+        <select className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
+            <option value="" disabled hidden>Experience</option>
+            <option value="Fresher">Fresher</option>
+            <option value="Junior Level">Junior Level</option>
+            <option value="Mid Level">Mid Level</option>
+            <option value="Senior Level">Senior Level</option>
+        </select>
     </div>
   )
 }
