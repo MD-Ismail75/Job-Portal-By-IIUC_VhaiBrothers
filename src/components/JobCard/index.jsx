ef954d350c7addc999f02c12314bc8f9d6eb563d
Rashedul Arefin Ifty <r.a.ifty2001@gmail.com>
2023-12-09 23:43:10 +0600

    JobCard (Design Add - Demo Data Show)

--- src/components/JobCard/index.jsx
+++ src/components/JobCard/index.jsx
@@ -7,20 +7,20 @@ function JobCard(props)
     const date1 = dayjs(Date.now());
     const diffInDays = date1.diff('2023-10-28','day');
   return (
-    <div>
-        <div>
-            <div>
-                <h1>Data Scientist</h1>
+    <div className='mx-40 mb-4'>
+        <div className='flex justify-between items-center px-6 py-4 bg-zinc-200 rounded-md border border-black shadow-lg hover:border-blue-500 hover:translate-y-1 hover:scale-103'>
+            <div className='flex flex-col items-start gap-3'>
+                <h1 className='text-lg font-semibold'>Data Scientist</h1>
                 <p>Full Time &#x2022; Fresher &#x2022; In-Office</p>
-                <div>
+                <div className='flex items-center gap-2'>
                     {skills.map((skill) => (
-                        <p>{skill}</p>
+                        <p key={skill} className='text-gray-500 py-1 px-2 rounded-md border border-black'>{skill}</p>
                     ))}
                 </div>
             </div>
-            <div>
-                <p> Posted {diffInDays} ago</p>
-                    <button>Apply</button>
+            <div className='flex items-center gap-4'>
+                <p className='text-gray-500'>Posted {diffInDays} ago</p>
+                    <button className='text-blue-500 border border-blue-500 px-10 py-2 rounded-md'>Apply</button>
             </div>
         </div>
     </div>
