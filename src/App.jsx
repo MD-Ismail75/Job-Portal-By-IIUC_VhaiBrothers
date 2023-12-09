4babce01173dcb143ec7ca011b29b3249a3177d1
Rashedul Arefin Ifty <r.a.ifty2001@gmail.com>
2023-12-09 20:02:12 +0600

    Jobcard Create

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar"
 import Header from "./components/Header"
 import SearchBar from "./components/SearchBar"
+import JobCard from "./components/JobCard"
 
 function App(){
 
@@ -9,6 +10,7 @@ function App(){
       <Navbar />
       <Header />
       <SearchBar />
+      <JobCard />
     </div>
   )
 }
---
+++ src/components/JobCard/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function JobCard() {
+  return (
+    <div>
+	jobcard
+    </div>
+  )
+}
+
+export default JobCard
