47c6aebb8044db7be57be9ad6554f7ac4a75b6e4
AfifHossain-1012 <afifhossain1012@gmail.com>
2023-12-11 15:56:02 +0600

    Modification of Navbar and Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>
-	header
+    <div className='mt-10 flex flex-col gap-5 items-center justify-center text-white'>
+      <h1 className='text-5xl font-bold'>Your ideal job awaits, start the search</h1>
+      <p className='text-xl'>Get latest job openings that best suits you!</p>
     </div>
   )
 }
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>
-	navbar
+    <div className='h-20 flex items-center w-full text-white'>
+      <div className='text-3xl pl-20 font-bold'>JobPortal.</div>
+
     </div>
   )
 }
