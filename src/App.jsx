34d9b22fd65734d0f006ecce680ce1cd9c52b220
AfifHossain-1012 <afifhossain1012@gmail.com>
2023-12-12 00:57:41 +0600

    Fetching data from database

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,36 @@ import SearchBar from "./components/SearchBar"
 import JobCard from "./components/JobCard"
 import jobData from "./JobDummyData"
 
+import { collection, query, orderBy, where, getDocs } from "firebase/firestore";
+import { db } from "./firebase.config"
+
 function App(){
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = async () => {
+    const tempJobs = []
+    const q = query(collection(db, "jobs"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((job) => {
+      
+      tempJobs.push({
+        ...job.data(),
+        id: job.id,
+      })
+    });
+    setJobs(tempJobs);
+  }
+
+  useEffect(() => {
+    fetchJobs()
+  }, [])
 
   return (
     <div>
       <Navbar />
       <Header />
       <SearchBar />
-      {jobData.map((job) => (
+      {jobs.map((job) => (
         <JobCard key={job.id} {...job}/>
         ))}
     </div>
