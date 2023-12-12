4a03ca3f6e9c8cdf3de965c4054e658e5b83e1ab
AfifHossain-1012 <afifhossain1012@gmail.com>
2023-12-13 01:57:56 +0600

    Clear Filter Button Setting

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,29 @@ import Navbar from "./components/Navbar"
 import Header from "./components/Header"
 import SearchBar from "./components/SearchBar"
 import JobCard from "./components/JobCard"
-import jobData from "./JobDummyData"
-
+import ValueBar from "./Components/ValueBar"
+import Footer from "./Components/Footer"
+// import jobData from "./JobDummyData"
+import { useEffect, useState } from "react"
 import { collection, query, orderBy, where, getDocs } from "firebase/firestore";
 import { db } from "./firebase.config"
-import ValueBar from "./components/ValueBar"
+// import Login from "./pages/Login"
+// import Signup from "./pages/Signup"
+// import Profile from "./pages/Profile"
 
-function App(){
+function App() {
   const [jobs, setJobs] = useState([]);
+  const [customSearch, setCustomSearch] = useState(false);
 
   const fetchJobs = async () => {
+    setCustomSearch(false);
     const tempJobs = []
     const jobsRef = query(collection(db, "jobs"));
     const q = query(jobsRef, orderBy("postedOn", "desc"));
     const req = await getDocs(q);
 
     req.forEach((job) => {
-
+      // console.log(doc.id, " => ", doc.data());
       tempJobs.push({
         ...job.data(),
         id: job.id,
@@ -46,19 +52,32 @@ function App(){
     setJobs(tempJobs);
   }
 
+
   useEffect(() => {
     fetchJobs()
   }, [])
 
+
   return (
     <div>
       <Navbar />
+      {/* <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes> */}
       <Header />
       <SearchBar fetchJobsCustom={fetchJobsCustom} />
+      {customSearch &&
+        <button onClick={fetchJobs} className="flex pl-[1250px] mb-2">
+          <p className="bg-blue-500 px-10 py-2 rounded-md text-white">Clear Filters</p>
+        </button>
+      }
       {jobs.map((job) => (
-        <JobCard key={job.id} {...job}/>
-        ))}
+        <JobCard key={job.id} {...job} />
+      ))}
       <ValueBar />
+      <Footer />
     </div>
   )
 }
